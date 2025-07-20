d4b442c784400a76eeb1b7f0f98aa597c7160079
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2025-07-21 00:10:07 +0500

    Add files via upload

---
+++ Celcius-1.asm
@@ -0,0 +1,31 @@
+; This program  find the celcius
+; C=F-32*5/9
+INCLUDE Irvine32.inc
+
+.data
+	faren dword 80
+	celcius dword ?
+	
+.code
+main PROC
+	
+	mov ebx,faren
+	sub ebx,32
+	
+	
+	mov ecx,ebx
+	mov edx,0
+	
+	mov ebx,9
+	mov eax,5
+	div ebx
+	
+	mov ebx,ecx
+	imul eax,ebx
+	mov celcius,eax
+	  
+	call DumpRegs
+
+	exit
+main ENDP
+END main
