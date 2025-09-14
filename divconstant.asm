da3cfadadd660cfa4cf57063adba348617fb0ce5
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2025-09-14 22:52:10 +0500

    Update divconstant.asm

--- divconstant.asm
+++ divconstant.asm
@@ -1,18 +1,13 @@
 ; This program divide 32-bit integer
 INCLUDE Irvine32.inc
-
 .code
 main PROC
-
 	mov eax,50
 	mov ebx,10
 	mov edx,0
 	idiv eax,ebx
 	call writeint
-	
-	
 	call DumpRegs
-
 	exit
 main ENDP
 END main
