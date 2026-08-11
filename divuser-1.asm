d168d062f7e8f2a19747e16f2a876da6a65b8ca0
MALIKA LIAQAT <159250050+malikaliaqat@users.noreply.github.com>
2026-08-11 03:26:52 -0700

    Add files via upload

---
+++ divuser-1.asm
@@ -0,0 +1,31 @@
+include irvine32.inc
+
+.data
+
+msg1 byte "Enter the 1st value=",0
+msg2 byte "Enter the 2nd value=",0
+msg3 byte "Division is=",0
+.code
+
+main proc
+
+	mov edx,offset msg1
+	call writestring
+	call readint
+	mov ebx,eax
+	call crlf          ;\n
+	mov edx,offset msg2
+	call writestring
+	call readint
+	
+	mov edx,0
+	div ebx
+	call crlf          ;\n
+	mov edx,offset msg3
+	call writestring
+	call writeint
+	call crlf          ;\n
+
+exit
+main endp
+end main
